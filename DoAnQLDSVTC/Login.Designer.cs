0a39c9ad092c967d7fc6af11829c2de29a42795c
ADMIN\ADMIN <leelamhair@gmail.com>
2025-11-14 01:32:46 +0700

    change layout form login

--- DoAnQLDSVTC/Login.Designer.cs
+++ DoAnQLDSVTC/Login.Designer.cs
@@ -35,6 +35,11 @@ namespace DoAnQLDSVTC
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.cmbKhoa = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.btnLogin = new System.Windows.Forms.Button();
             this.cbStudent = new System.Windows.Forms.CheckBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
@@ -43,11 +48,26 @@ namespace DoAnQLDSVTC
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel6.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel12.SuspendLayout();
+            this.panel14.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -63,41 +83,93 @@ namespace DoAnQLDSVTC
             // 
             // cmbKhoa
             // 
-            this.cmbKhoa.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbKhoa.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.cmbKhoa.FormattingEnabled = true;
-            this.cmbKhoa.Location = new System.Drawing.Point(174, 308);
+            this.cmbKhoa.Location = new System.Drawing.Point(50, 29);
             this.cmbKhoa.Name = "cmbKhoa";
-            this.cmbKhoa.Size = new System.Drawing.Size(151, 21);
+            this.cmbKhoa.Size = new System.Drawing.Size(300, 21);
             this.cmbKhoa.TabIndex = 1;
             this.cmbKhoa.SelectedIndexChanged += new System.EventHandler(this.cmbKhoa_SelectedIndexChanged);
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.btnLogin);
-            this.panel1.Controls.Add(this.cbStudent);
-            this.panel1.Controls.Add(this.txtPassword);
-            this.panel1.Controls.Add(this.txtUserName);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Controls.Add(this.cmbKhoa);
+            this.panel1.Controls.Add(this.panel14);
+            this.panel1.Controls.Add(this.panel13);
+            this.panel1.Controls.Add(this.panel12);
+            this.panel1.Controls.Add(this.panel11);
+            this.panel1.Controls.Add(this.panel10);
+            this.panel1.Controls.Add(this.panel9);
+            this.panel1.Controls.Add(this.panel8);
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(676, 0);
+            this.panel1.Location = new System.Drawing.Point(678, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(402, 708);
+            this.panel1.Size = new System.Drawing.Size(400, 708);
             this.panel1.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 192);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(400, 70);
+            this.panel4.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(26, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CHÀO MỪNG BẠN ĐẾN HỆ THỐNG";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 74);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(400, 118);
+            this.panel3.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(156, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(98, 97);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 74);
+            this.panel2.TabIndex = 0;
             // 
             // btnLogin
             // 
             this.btnLogin.BackColor = System.Drawing.Color.Brown;
+            this.btnLogin.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnLogin.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnLogin.Location = new System.Drawing.Point(105, 465);
+            this.btnLogin.Location = new System.Drawing.Point(100, 0);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(213, 41);
+            this.btnLogin.Size = new System.Drawing.Size(200, 41);
             this.btnLogin.TabIndex = 11;
             this.btnLogin.Text = "Đăng Nhập";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -106,35 +178,39 @@ namespace DoAnQLDSVTC
             // cbStudent
             // 
             this.cbStudent.AutoSize = true;
+            this.cbStudent.Dock = System.Windows.Forms.DockStyle.Right;
             this.cbStudent.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbStudent.Location = new System.Drawing.Point(174, 419);
+            this.cbStudent.Location = new System.Drawing.Point(262, 0);
             this.cbStudent.Name = "cbStudent";
-            this.cbStudent.Size = new System.Drawing.Size(88, 23);
+            this.cbStudent.Size = new System.Drawing.Size(88, 25);
             this.cbStudent.TabIndex = 10;
             this.cbStudent.Text = "Sinh Viên";
             this.cbStudent.UseVisualStyleBackColor = true;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(174, 387);
+            this.txtPassword.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtPassword.Location = new System.Drawing.Point(50, 30);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(151, 20);
+            this.txtPassword.Size = new System.Drawing.Size(300, 20);
             this.txtPassword.TabIndex = 9;
             this.txtPassword.Text = "123456";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(174, 347);
+            this.txtUserName.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtUserName.Location = new System.Drawing.Point(50, 30);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(151, 20);
+            this.txtUserName.Size = new System.Drawing.Size(300, 20);
             this.txtUserName.TabIndex = 8;
             this.txtUserName.Text = "lnkt";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Top;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 12F);
-            this.label5.Location = new System.Drawing.Point(91, 389);
+            this.label5.Location = new System.Drawing.Point(50, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(71, 19);
             this.label5.TabIndex = 7;
@@ -143,8 +219,9 @@ namespace DoAnQLDSVTC
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 12F);
-            this.label4.Location = new System.Drawing.Point(91, 348);
+            this.label4.Location = new System.Drawing.Point(50, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(73, 19);
             this.label4.TabIndex = 6;
@@ -153,8 +230,9 @@ namespace DoAnQLDSVTC
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F);
-            this.label3.Location = new System.Drawing.Point(91, 308);
+            this.label3.Location = new System.Drawing.Point(50, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 19);
             this.label3.TabIndex = 5;
@@ -164,31 +242,104 @@ namespace DoAnQLDSVTC
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(96, 217);
+            this.label2.Location = new System.Drawing.Point(63, 39);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(276, 24);
             this.label2.TabIndex = 4;
             this.label2.Text = "QUẢN LÝ ĐIỂM SINH VIÊN";
             // 
-            // label1
+            // panel5
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(52, 184);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(359, 24);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "CHÀO MỪNG BẠN ĐẾN HỆ THỐNG";
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 262);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(400, 35);
+            this.panel5.TabIndex = 3;
             // 
-            // pictureBox2
+            // panel6
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(153, 62);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(94, 95);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.panel6.Controls.Add(this.label3);
+            this.panel6.Controls.Add(this.cmbKhoa);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Location = new System.Drawing.Point(0, 297);
+            this.panel6.Name = "panel6";
+            this.panel6.Padding = new System.Windows.Forms.Padding(50, 0, 50, 0);
+            this.panel6.Size = new System.Drawing.Size(400, 50);
+            this.panel6.TabIndex = 4;
+            // 
+            // panel7
+            // 
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 347);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(400, 25);
+            this.panel7.TabIndex = 5;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.label4);
+            this.panel8.Controls.Add(this.txtUserName);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel8.Location = new System.Drawing.Point(0, 372);
+            this.panel8.Name = "panel8";
+            this.panel8.Padding = new System.Windows.Forms.Padding(50, 0, 50, 0);
+            this.panel8.Size = new System.Drawing.Size(400, 50);
+            this.panel8.TabIndex = 6;
+            // 
+            // panel9
+            // 
+            this.panel9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel9.Location = new System.Drawing.Point(0, 422);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(400, 25);
+            this.panel9.TabIndex = 7;
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.label5);
+            this.panel10.Controls.Add(this.txtPassword);
+            this.panel10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel10.Location = new System.Drawing.Point(0, 447);
+            this.panel10.Name = "panel10";
+            this.panel10.Padding = new System.Windows.Forms.Padding(50, 0, 50, 0);
+            this.panel10.Size = new System.Drawing.Size(400, 50);
+            this.panel10.TabIndex = 8;
+            // 
+            // panel11
+            // 
+            this.panel11.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel11.Location = new System.Drawing.Point(0, 497);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(400, 25);
+            this.panel11.TabIndex = 9;
+            // 
+            // panel12
+            // 
+            this.panel12.Controls.Add(this.cbStudent);
+            this.panel12.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel12.Location = new System.Drawing.Point(0, 522);
+            this.panel12.Name = "panel12";
+            this.panel12.Padding = new System.Windows.Forms.Padding(50, 0, 50, 0);
+            this.panel12.Size = new System.Drawing.Size(400, 25);
+            this.panel12.TabIndex = 10;
+            // 
+            // panel13
+            // 
+            this.panel13.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel13.Location = new System.Drawing.Point(0, 547);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(400, 50);
+            this.panel13.TabIndex = 11;
+            // 
+            // panel14
+            // 
+            this.panel14.Controls.Add(this.btnLogin);
+            this.panel14.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel14.Location = new System.Drawing.Point(0, 597);
+            this.panel14.Name = "panel14";
+            this.panel14.Padding = new System.Windows.Forms.Padding(100, 0, 100, 0);
+            this.panel14.Size = new System.Drawing.Size(400, 50);
+            this.panel14.TabIndex = 12;
             // 
             // Login
             // 
@@ -203,8 +354,19 @@ namespace DoAnQLDSVTC
             this.Load += new System.EventHandler(this.Login_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel10.ResumeLayout(false);
+            this.panel10.PerformLayout();
+            this.panel12.ResumeLayout(false);
+            this.panel12.PerformLayout();
+            this.panel14.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -224,5 +386,18 @@ namespace DoAnQLDSVTC
         private Label label5;
         private Button btnLogin;
         private CheckBox cbStudent;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel6;
+        private Panel panel5;
+        private Panel panel13;
+        private Panel panel12;
+        private Panel panel11;
+        private Panel panel10;
+        private Panel panel9;
+        private Panel panel8;
+        private Panel panel7;
+        private Panel panel14;
     }
 }
